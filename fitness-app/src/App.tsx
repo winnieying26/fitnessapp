3effdc45af953c1469dcd6dd75f8bea48702c0c5
winnieying26 <winniequan55@gmail.com>
2024-12-11 17:50:38 -0500

    create the navbar for web mode

--- fitness-app/src/App.tsx
+++ fitness-app/src/App.tsx
@@ -1,10 +1,16 @@
-
+import Navbar from "@/scenses/navbar"
+import { useState } from "react"
+import { SelectedPage } from "./shared/types";
 
 function App() {
-  
+  const [selectedPage, setSelectedPage] =useState<SelectedPage>(SelectedPage.Home); 
 
   return (
-   <div className="app">App</div>
+   <div className="app bg-gray-20">
+    <Navbar 
+    selectedPage={selectedPage}
+    setSelectedPage={setSelectedPage}/>
+   </div>
   )
 }
 
